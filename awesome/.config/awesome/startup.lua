f6d62aa5368fd4b76ace8ea38c54289cb00d50d1
daneofmanythings <dhcjensen@gmail.com>
2024-12-06 09:27:24 -0800

    Removed debian specific variables.

--- awesome/.config/awesome/startup.lua
+++ awesome/.config/awesome/startup.lua
@@ -1,7 +1,7 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("picom")
-awful.spawn.with_shell("redshift")
+-- awful.spawn.with_shell("redshift")
 -- awful.spawn.with_shell("nitrogen --restore")
 -- awful.spawn.with_shell(
 -- 	'nvidia-settings --assign CurrentMetaMode="1920x1080_144 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"'
